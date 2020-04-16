e502a9acbc68643a0df38809cef07846c284e88c
PavloSokol <34427149+PavloSokol@users.noreply.github.com>
2020-04-16 14:24:34 +0300

    Update server.js

--- 1LB_NODEJS/server.js
+++ 1LB_NODEJS/server.js
@@ -17,14 +17,14 @@ app.get("/add", function(request, response){
     response.send("<h1>Привіт Express!</h1>");
 });
 
-app.use(function(req, res, next){
+app.use( function(req, res, next){
     res.status(404);
     log.debug('Not found URL: %s',req.url);
     res.send({ error: 'Not found' });
     return;
     });
     
-app.use(function(err, req, res, next){
+app.use ( function(err, req, res, next){
     res.status(err.status || 500);
     log.error('Internal error(%d): %s',res.statusCode,err.message);
     res.send({ error: err.message });
