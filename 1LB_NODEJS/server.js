9d26683cede45a072b2b348ce009e1cebacb24cf
PavloSokol <34427149+PavloSokol@users.noreply.github.com>
2020-04-16 14:27:19 +0300

    Update server.js

--- 1LB_NODEJS/server.js
+++ 1LB_NODEJS/server.js
@@ -12,45 +12,45 @@ app.listen(1337, function(){
     console.log('Express server listening on port 1337');
     }); 
 
-app.get("/add", function(request, response){
+app.get("/add", function(request, response) {
 
     response.send("<h1>Привіт Express!</h1>");
 });
 
-app.use( function(req, res, next){
+app.use( function(req, res, next) {
     res.status(404);
     log.debug('Not found URL: %s',req.url);
     res.send({ error: 'Not found' });
     return;
     });
     
-app.use ( function(err, req, res, next){
+app.use (function(err, req, res, next) {
     res.status(err.status || 500);
     log.error('Internal error(%d): %s',res.statusCode,err.message);
     res.send({ error: err.message });
      return;
     }); 
 
-app.get( '/ErrorExample', function(req, res, next){
+app.get ('/ErrorExample', function(req, res, next) {
         next(new Error('Random error!'));
         }); 
 
-        app.get( '/api/articles', function(req, res) {
+        app.get ('/api/articles', function(req, res) {
             res.send('This is not implemented now');
             });
 
-            app.post( '/api/articles', function(req, res) {
+            app.post('/api/articles', function(req, res) {
              res.send('This is not implemented now');
             });
 
-            app.get( '/api/articles/:id', function(req, res) {
+            app.get ('/api/articles/:id', function(req, res) {
             res.send('This is not implemented now');
             });
 
-            app.put( '/api/articles/:id', function (req, res){
+            app.put ('/api/articles/:id', function (req, res) {
             res.send('This is not implemented now');
             });
 
-            app.delete( '/api/articles/:id', function (req, res){
+            app.delete('/api/articles/:id', function (req, res) {
             res.send('This is not implemented now');
             });
